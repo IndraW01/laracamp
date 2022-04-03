900040a385b1562e07696deef12a4f8e6361f663
IndraW01 <indraakmal08@gmail.com>
2022-04-03 12:28:32 +0800

    finish migration, camp, camp benefit, checkout

---
+++ database/migrations/2022_04_03_041738_create_checkouts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Camp;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained('users');
+            $table->foreignIdFor(Camp::class)->constrained('camps');
+            $table->string('card_number');
+            $table->date('expired');
+            $table->string('cvc', 3);
+            $table->boolean('is_paid')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkouts');
+    }
+};
