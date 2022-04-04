75ddf8a07426331b30e4e2931dc83e3630c1f6ab
IndraW01 <indraakmal08@gmail.com>
2022-04-04 15:45:06 +0800

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
 
 Route::get('/checkout', function () {
     return view('checkout');
@@ -17,3 +24,13 @@ Route::get('/checkout', function () {
 Route::get('/success-checkout', function () {
     return view('success-checkout');
 })->name('success.checkout');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
